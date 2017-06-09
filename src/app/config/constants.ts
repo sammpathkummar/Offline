07f47e7f3b4146ad1de8f4d25a1c4e9041387ab9
sammpathkummar <sampath415@gmail.com>
2017-06-09 23:24:07 +0800

    Add files via upload

---
+++ src/app/config/constants.ts
@@ -0,0 +1,2 @@
+export const DREAMFACTORY_API_KEY: string = 'a7b6d4c78c4f5280f861fd14d62b01f955f867f98f2bc4bf87ae8c71689263d7';
+export const DREAMFACTORY_INSTANCE_URL: string = 'http://api.zen.com.my/';
